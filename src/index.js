447937d2c37d84a8a00786e374eea0574eb5c2af
Ellis Yoo <ellisyoo@elliss-mbp.lan>
2023-05-09 16:23:49 -0700

    connect db to mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 const PORT = process.env.PORT || 3000;
+
+require('dotenv').config()
 const app = require("express")();
 const server = require("http").createServer(app);
 const { Server } = require("socket.io");
-const io = require("socket.io")(server);
+// const io = require("socket.io")(server);
+const io = new Server(server);
+
+const User = require('./models/user');
+
+const DATABASE = process.env.ATLAS_URI
+const mongoose = require('mongoose');
+mongoose.connect(DATABASE, { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '))
+db.once('open', function(){
+    console.log('connected to database!');
+})
 
 app.get("/", (req, res) => {
     res.sendFile("./index.html", { root: __dirname });
@@ -13,8 +27,6 @@ io.on("connection", (socket) => {
         io.emit("chat message", msg)
     })
 })
-
-
 
 server.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: {type: String},
-    nickname: {type: String},
+    email: { type: String },
+    nickname: { type: String },
     favorites: [], // favorite chat room
     friends: [],
-    created: { type: Date},
+    created: { type: Date },
 })
 
 const Users = mongoose.model('User', userSchema);
 
-module.exports(Users);
+module.exports = Users;
